e16c1044f4f7c9a3ab8c5ddcbebfa2c404ebc05e
siddzzshetty <siddhidshetty@gmail.com>
2025-04-04 18:08:29 +0530

    no changes

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
 import os 
 from langchain_groq import ChatGroq
-
 GROQ_API_KEY = "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"
 os.environ["GROQ_API_KEY"] = GROQ_API_KEY
 chat_model = ChatGroq(model_name="llama-3.3-70b-versatile", api_key=GROQ_API_KEY)
